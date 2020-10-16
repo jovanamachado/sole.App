a9cea02faaea2a23215d75bca18591ddc45c0084
jovanamachado <jovanaa_@outlook.com>
2020-10-16 15:24:23 -0300

    Funcionalidade do botão entrar

--- app/src/main/java/com/example/soleapp/MainActivity.java
+++ app/src/main/java/com/example/soleapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.soleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnEntrarprog = (Button) findViewById(R.id.btnEntrar);
+        btnEntrarprog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, tela2.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/soleapp/tela2.java
+++ app/src/main/java/com/example/soleapp/tela2.java
@@ -3,6 +3,9 @@ package com.example.soleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class tela2 extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class tela2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela2);
+
+        EditText edtInsira_prog = (EditText) findViewById(R.id.edtInsira);
+        Button btninsira_prog = (Button) findViewById(R.id.btnInsira);
+
+        btninsira_prog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float tela2;
+                //PAREI AQUI// TENTANDO DESENVOLVER COMO O RESULTADO DA "TELA 2" IRÁ APARECER NA "TELA 3"//
+            }
+        });
+
     }
 }

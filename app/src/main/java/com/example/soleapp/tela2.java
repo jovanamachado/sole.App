50f75dd555f39730b010d8ce72e1d8642bcf8198
Dione Carlos <catalina@iMac-de-Dione.local>
2020-10-17 18:10:56 -0300

    Adição do resultado em litros

--- app/src/main/java/com/example/soleapp/tela2.java
+++ app/src/main/java/com/example/soleapp/tela2.java
@@ -2,6 +2,7 @@ package com.example.soleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,14 +15,36 @@ public class tela2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela2);
 
-        EditText edtInsira_prog = (EditText) findViewById(R.id.edtInsira);
+        final EditText edtInsira_prog = (EditText) findViewById(R.id.edtInsira);
         Button btninsira_prog = (Button) findViewById(R.id.btnInsira);
 
         btninsira_prog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float tela2;
-                //PAREI AQUI// TENTANDO DESENVOLVER COMO O RESULTADO DA "TELA 2" IRÁ APARECER NA "TELA 3"//
+                final float tela2;
+
+
+         final EditText edtInsira;
+         Button btnInsira;
+
+         edtInsira = (EditText) findViewById(R.id.edtInsira);
+         btnInsira = (Button) findViewById(R.id.btnInsira);
+
+     btnInsira.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             Intent intent = new Intent(tela2.this, tela3.class);
+
+             float km, l;
+             km = Float.parseFloat(edtInsira_prog.getText().toString());
+             l = km / 13;
+
+
+             intent.putExtra("Chave", (String.valueOf(l)));
+             startActivity(intent);
+             finish();
+         }
+     });
             }
         });
 
--- app/src/main/java/com/example/soleapp/tela3.java
+++ app/src/main/java/com/example/soleapp/tela3.java
@@ -3,12 +3,29 @@ package com.example.soleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class tela3 extends AppCompatActivity {
+
+
+
+    EditText edtResult;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela3);
+
+
+
+        edtResult = (EditText) findViewById(R.id.edtResult);
+        String valor = getIntent(). getStringExtra("Chave");
+        edtResult.setText(valor);
+
+
+
     }
 }
